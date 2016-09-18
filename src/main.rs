967357f43c07831eb6cd42413347804d8a835b63
Dmitrij Fedorenko <c0va23@gmail.com>
2016-09-18 22:21:00 +0300

    Improve parse source file

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,32 @@ use std::time::{
     Instant,
 };
 
+fn parse_line(line: String) -> Result<(String, u32), String> {
+    let mut line_parts = line.splitn(2, ",");
+    let series = line_parts.next().unwrap().to_string();
+    let number_str = line_parts.next().unwrap();
+    match number_str.parse() {
+        Ok(number) => Ok((series, number)),
+        Err(err) => Err(format!("{}", err)),
+    }
+}
+
 fn main() {
     let instant = Instant::now();
-    let file = File::open("expass.csv").unwrap();
+    let file = File::open("data.csv").unwrap();
     let reader = BufReader::new(file);
     let mut passports = BTreeMap::new();
     for buffer in reader.lines() {
         let line = buffer.unwrap();
-        let series: u16 = line[0..3].parse().unwrap();
-        let number: u32 = line[5..11].parse().unwrap();
-        passports.entry(series)
-                .or_insert(BTreeSet::new())
-                .insert(number);
+        match parse_line(line.to_string()) {
+            Ok((series, number)) => {
+                passports.entry(series)
+                    .or_insert(BTreeSet::new())
+                    .insert(number);
+            },
+            Err(err) =>
+                println!("Error parse line {} with error: {}", line, err),
+        }
     }
     let parse_duration = instant.elapsed();
     println!("Parse duration {:?}", parse_duration.as_secs());
