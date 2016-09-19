36c40789fe1478e918105218422a39acb0a4d2ec
Dmitrij Fedorenko <c0va23@gmail.com>
2016-09-19 23:16:13 +0300

    Add simple http-server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 extern crate expass;
+extern crate hyper;
+
+use hyper::server::{Server, Request, Response};
+use hyper::uri::RequestUri;
 
 fn main() {
     let database = expass::Database::new("expass.csv");
@@ -13,4 +17,19 @@ fn main() {
             number,
         ),
     );
+
+    Server::http("0.0.0.0:8000").unwrap().handle(move |req: Request, res: Response| {
+        if let RequestUri::AbsolutePath(path) = req.uri {
+            let mut path_parts = path.splitn(3, '/').skip(1); // Skip empty part
+            let series = path_parts.next().expect("Series not found");
+            let number = path_parts.next().expect("Number not found")
+                                   .parse::<u32>().expect("Invalid number");
+            let exists = database.is_exist(series.to_string(), number);
+            println!("{} {} is {}", series, number, exists);
+            res.send(format!("{}", exists).as_bytes()).unwrap();
+        } else {
+            println!("{} is not AbsolutePath", req.uri);
+        }
+
+    }).unwrap();
 }
